d7a70f9c8a29c8bfd056d6339dce0bb9f5c0f6c9
GitHub Action <action@github.com>
2026-03-18 02:55:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/c211007/androidstudiokoreanlanguagepack/MyBundle.kt
+++ src/main/kotlin/com/github/c211007/androidstudiokoreanlanguagepack/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.c211007.androidstudiokoreanlanguagepack
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/c211007/androidstudiokoreanlanguagepack/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/c211007/androidstudiokoreanlanguagepack/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.c211007.androidstudiokoreanlanguagepack.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
